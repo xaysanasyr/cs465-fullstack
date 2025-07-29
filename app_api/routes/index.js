184715e1e5c558ac991b7fc9c32586650c4648ca
xaysanasyr <rikki.xaysanasy@snhu.edu>
2025-07-29 16:35:02 -0500

    Module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express"); // express app
+const router = express.Router(); // router logic
+
+// import the controllers to route
+const tripsController = require("../controllers/trips");
+
+// define route for trips endpoints
+router.route("/trips").get(tripsController.tripsList);
+// get method routes tripsFindByCode - requires params
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+
+module.exports = router;
